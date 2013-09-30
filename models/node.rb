1d49246bd522fc92be7b9a1fd682f236c6ef9772
Michael Hall <michael.hall@puppetlabs.com>
2013-09-29 17:28:03 -0700

    Updated and documented models

--- models/node.rb
+++ models/node.rb
@@ -1,21 +1,36 @@
-class Node < ActiveRecord::Base
 
+class Node < ActiveRecord::Base
+  require File.expand_path("../../config/legato.rb", __FILE__)
   belongs_to :author
   has_many :stats
-  
+
+  # taxonomical considerations  
   scope :no_funnel, where(:taxo_funnel =>  [nil,0]) 
   scope :no_theme,  where(:taxo_theme =>   [nil,0]) 
   scope :no_source, where(:taxo_source =>    [nil,0])
   scope :no_type,  where(:taxo_type =>    [nil,0])
+
+  # finding things by period or time
   scope :this_quarter, where('pub_date >= ? AND pub_date <= ?', Date.today.beginning_of_quarter, Date.today.end_of_quarter)
-  scope :by_month, lambda { |month,year|  
-    where('extract(month from pub_date) = ? AND extract(year from pub_date) = ?',month,year)
-  }
+  scope :by_month, lambda { |month|  where('extract(month from pub_date) = ?',month)}
+  scope :by_year, lambda { |year| where('extract(year from pub_date) = ?',year) }
+  scope :by_quarter, lambda { |quarter| where('extract(month from pub_date) <= ? AND extract(month from pub_date) >= ?',(quarter * 3 ), (quarter * 3 -2)) }
+  scope :past_ten, where('pub_date > ?', Date.today - 14.days)  
   
   
+  # Find by "employee", "staff_writer" etc. This will fall away once we have the taxonomy back-ported
+  def self.by_source(source)
+    Node.includes(:author).where('authors.kind = ?',source)
+  end
+  
+  # Return the age of a node in days
+  def age
+    return (Date.today - self.pub_date).to_i
+  end
+  
+  # Return the pageviews of a node, generally gathered at 2,7,14 30 and every 30 days thereafter
   def pageviews(period)
     stat = self.stats.find(:first, :conditions => ['period = ?', period])
-    
     if stat
       return stat.pageviews
     else
@@ -23,17 +38,41 @@ class Node < ActiveRecord::Base
     end
   end
   
+  # Return the bounce rate of a node at the given age
   def bounce_rate(period)
      stat = self.stats.where(:period => period).first
      return stat.bounce_rate
   end
 
+  # Return the visits to a node at the given age
   def visits(period)
      stat = self.stats.where(:period => period).first
      return stat.visits
   end
 
+  # Return how many stats of a given period exist
+  # Should never be anything besides 1 or 0
   def stats_from_period(period)
     return self.stats.where(:period => period).count
   end
+  
+  # Determine if the lifetime stats for a given period are fresh
+  def has_current_lifetime?
+    if stat = self.stats.find(:first, :conditions => ["kind = ?","lifetime"])
+      return stat.current?
+    else
+      return false
+    end
+  end
+  
+  # Show lifetime views for a stat as of last general gather
+  def lifetime_views
+    return self.stats.where(:kind => "lifetime").first.pageviews
+  end
+  
+  # Show lifetime views for a stat as of right now
+  def realtime_lifetime
+    get_realtime_views(self)
+  end
+  
 end
--- models/stat.rb
+++ models/stat.rb
@@ -1,7 +1,35 @@
 class Stat < ActiveRecord::Base
-  
   belongs_to :node
 
+  # Find all the "lifetime" stats 
+  scope :lifetime, where(:kind => "lifetime")
+
+
+  # When was the stat last updated in days? Used to find "lifetime" stats 
+  # that need to be refreshed via the current? method. We could move this 
+  # into the current? method
+  def age
+    return (Date.today - self.updated_at.to_date).to_i
+  end
+
+  # Determine if a view is "current" depending on the parent node's age
+  # We use this to look for "expired" lifetime views every now and then
+  def current?
+    if self.node.age > 365 && self.age > 30
+      return false
+    elsif self.node.age > 180 && self.age > 14 
+      return false
+    elsif self.node.age >= 30 && self.age > 7 
+      return false
+    elsif self.node.age < 30 && self.age > 2
+      return false
+    else 
+      return true
+    end
+  end
+
+  # This method helps us find stats that aren't complete for whatever reason (most likely because the
+  # stats being gathered have changed and we haven't updated all the gathered items).
   def complete? 
     if self.pageviews && self.bounce_rate && self.visits 
       true
@@ -9,5 +37,6 @@ class Stat < ActiveRecord::Base
       false
     end
   end
-    
+
+
 end
