547f8b268ebec0a24808b43a5d20982ac4173fb5
Michael Hall <michael.hall@puppetlabs.com>
2013-12-17 07:53:27 -0800

    Fixing post_refers to pull in for intervals, not just lifetime

--- scripts/get_post_refers.rb
+++ scripts/get_post_refers.rb
@@ -1,24 +1,28 @@
+#!/usr/bin/env ruby 
+
 require File.expand_path("../../config/boot.rb", __FILE__)
 require File.expand_path("../../config/legato.rb", __FILE__)
 
 report_intervals = [2,7,14,30,60,90,180,365]
 
-# report_intervals.each do |interval|
-#   puts "Checking #{interval}-day refer stats on all nodes ..."
-#   
-#   Node.all.each do |n|
-#     next if n.has_stats?(interval) || n.issue == true
-#     if n.pub_date < Date.today - interval.days - 1.day 
-#       puts "#{n.title} (#{interval}) id: #{n.id}"
-#       update_node_refers(n,interval,"period")
-#     end
-#   end
-# end
+report_intervals.each do |interval|
+  puts "Checking #{interval}-day refer stats on all nodes ..."
+  
+  Node.all.each do |n|
+    next if n.has_refers?(interval)
+    if n.pub_date < Date.today - interval.days - 1.day 
+      puts "#{n.title} (#{interval}) id: #{n.id}"
+      update_node_refers(n,interval,"period")
+    end
+  end
+end
 
 puts "Checking lifetime refers on all nodes ..."
 
-Node.all.each do |n|
-  next if n.has_current_lifetime_refers? #|| n.pub_date < Date.today - 30.days
+nodes = Node.find(:all, :conditions => ['pub_date > ?', Date.today - 30.days])
+
+nodes.each do |n|
+  next if n.has_current_lifetime_refers? || n.pub_date <  Date.today - 30.days
   age = (Date.today - n.pub_date).to_i
   puts "Update #{n.title} ... "
   update_node_refers(n,age,"lifetime")
